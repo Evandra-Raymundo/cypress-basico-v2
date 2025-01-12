1b0a7ff58f665fbd0420c4ed69f299a8a5a6d76b
Evandra-Raymundo <evenfashion@gmail.com>
2025-01-12 17:38:40 -0300

    Configura cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,9 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  projectId: "t4p7oo",
   viewportHeight: 880,
   viewportWidth: 1280,
   e2e: {},
-  //video: true deligado pois era pra ser usado apenas em um exercício
+   //video: true deligado pois era pra ser usado apenas em um exercício
 })
